e4730bd030969e3937a6367369bb5eb0ce5636ff
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-01-03 19:03:01 -0500

    fix: scoreScrabble, fet: isPalindome with while loop, comments added to findFrequency.

--- submission-template.js
+++ submission-template.js
@@ -14,48 +14,63 @@ const findSum = function(array) {
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
   let mostFreq = 0;
-  let leastFreq = 9999;
+  let leastTemp = Infinity;
   let mostTemp = 0;
   let most = "";
   let least = "";
+  let comp1 = "";
+  let comp2 = "";
 
   for (let i=0; i<array.length-1; i++) {
-    let comp1 = array[i];
+    //select string to be compared to all strings in array
+    comp1 = array[i];
+    //reset frequency counter for each iteration 
     mostFreq = 0;
-    //console.log(most);
 
     for (let j=0; j<array.length; j++) {
-      let comp2 = array[j];
+      //select second string to be compared to comp1
+      comp2 = array[j];
+      //find identical strings, increment counter if true
       if (comp1 == comp2) {
         mostFreq++;
       } 
     }
+    //keep track of highest frequency by updating mostTemp
     if (mostFreq > mostTemp) {
       mostTemp = mostFreq;
       most = comp1;
 
     }
-    if (mostFreq < leastFreq) {
-      leastFreq = mostFreq;
+    //keep track of lowest frequency by updating leastTemp
+    if (mostFreq < leastTemp) {
+      leastTemp = mostFreq;
       least = comp1;
-      //console.log(least);
-
     }
   }
   return {most: most, least: least};
 };
 
-//array = ['a', 'banana', 'a', 'd', 'd', 'd', 'd', 'b', 'b', 'c', 'c'] ;
+//array = ['a', 'banana', 'a', 'd', 'd', 'd', 'd', 'b', 'b', 'c', 'c'];
 //result = findFrequency(array);
 //console.log(result);
 
 const isPalindrome = function(str) {
   // your code here - don't forget to return a boolean!
   //edge case - odd number of characters
-  for (let i=0; i<Math.floor(str.length/2); i++) {
-    if (str[i] != str[(str.length-1)-i]) {
+  //for (let i=0; i<Math.floor(str.length/2); i++) {
+    //if (str[i] != str[(str.length-1)-i]) {
+      //return false;
+    //}
+  //}
+
+  start = 0;
+  end = str.length-1;
+  while(start != end){
+    if (str[start] != str[end]) {
       return false;
     }
+    start++;
+    end--;
   }
   return true;
 };
@@ -102,29 +117,34 @@ const removeParenth = function(str) {
 const scoreScrabble = function(str) {
   // your code here - don't forget to return a number!
   let score = 0;
+
   for (let i=0; i<str.length; i++) {
-    if (str[i] == ('a' || 'e' || 'i' || 'o' || 'u' || 'l' || 'n' || 'r' || 's' || 't')){
+    if (['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'].includes(str[i])){
       score += 1;
     }
-    if (str[i] == ('d' || 'g')){
+    if (['d', 'g'].includes(str[i])){
       score += 2;
     }
-    if (str[i] == ('b' || 'c' || 'm' || 'p')){
+    if (['b', 'c', 'm', 'p'].includes(str[i])){
       score += 3;
     }
-    if (str[i] == ('f' || 'h' || 'v' || 'w' || 'y')){
+    if (['f', 'h', 'v', 'w', 'y'].includes(str[i])){
       score += 4;
     }
-    if (str[i] == ('k')){
+    if (['k'].includes(str[i])){
       score += 5;
     }
-    if (str[i] == ('j' || 'x')){
+    if (['j', 'x'].includes(str[i])){
       score += 8;
     }
-    if (str[i] == ('q' || 'z')){
+    if (['q', 'z'].includes(str[i])){
       score += 10;
     }
   }
   
 return score;
 };
+
+//str = 'caziques';
+//result = scoreScrabble(str);
+//console.log(result);
